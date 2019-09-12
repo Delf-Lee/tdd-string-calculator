9a2dbaf7cd81859affbbfdb5845c42bbeb3c77de
Delf-Lee <ttjs2008@naver.com>
2019-09-12 23:40:58 +0900

    Separate spring parser

--- calc/src/main/java/Calculator.java
+++ calc/src/main/java/Calculator.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
  * @author delf
  */
 public class Calculator {
-	private static final String[] OPERATORS = {"+", "-", "*", "/"};
-
 	public int calc(String formula) {
 
-		char operator = getOperand(formula);
-		int[] operands = getOperands(formula);
+		StringParser stringParser = new StringParser();
+
+		char operator = stringParser.getOperand(formula);
+		int[] operands = stringParser.getOperands(formula);
 
 		switch (operator) {
 			case '+':
@@ -23,24 +23,6 @@ public class Calculator {
 		}
 
 		return 0;
-	}
-
-	private int[] getOperands(String formula) {
-		StringBuilder operands = new StringBuilder();
-		for (char ch : formula.toCharArray()) {
-			operands.append(Character.isDigit(ch) ? ch : " ");
-		}
-		String[] op = operands.toString().split(" ");
-		return Arrays.stream(op).mapToInt(Integer::parseInt).toArray();
-	}
-
-	private char getOperand(String formula) {
-		for (String ch : OPERATORS) {
-			if (formula.contains(ch)) {
-				return ch.charAt(0);
-			}
-		}
-		throw new IllegalArgumentException();
 	}
 
 }
--- calc/src/main/java/StringParser.java
+++ calc/src/main/java/StringParser.java
@@ -1,6 +1,26 @@
+import java.util.Arrays;
+
 /**
  * @author delf
  */
 public class StringParser {
+	public static final String[] OPERATORS = {"+", "-", "*", "/"};
+	public int[] getOperands(String formula) {
+		StringBuilder operands = new StringBuilder();
+		for (char ch : formula.toCharArray()) {
+			operands.append(Character.isDigit(ch) ? ch : " ");
+		}
+		String[] op = operands.toString().split(" ");
+		return Arrays.stream(op).mapToInt(Integer::parseInt).toArray();
+	}
+
+	public char getOperand(String formula) {
+		for (String ch : OPERATORS) {
+			if (formula.contains(ch)) {
+				return ch.charAt(0);
+			}
+		}
+		throw new IllegalArgumentException();
+	}
 
 }
